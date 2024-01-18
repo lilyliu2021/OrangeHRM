4e64d04d4ec27f0b95751cc143b75c57ef92fe7d
lilyliu2021 <lily.liu201209@gmail.com>
2024-01-17 16:32:45 -0800

    added POM for time report page

--- cypress/e2e/timeReport.cy.js
+++ cypress/e2e/timeReport.cy.js
@@ -1,7 +1,9 @@
+import TimeReport from "../support/pageObjects/TimeReport";
+
 describe("Project Time Report Testing", () => {
   it("Time reports", () => {
-    cy.visit('/');
-    cy.fixture("admin").then((data) => {
+    cy.visit("/");
+    cy.fixture("loginInfo").then((data) => {
       cy.login(data.username, data.password);
       cy.get(
         ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
@@ -14,43 +16,54 @@ describe("Project Time Report Testing", () => {
       );
     });
     cy.get('li[class*="topbar"]').contains("Reports").click();
-    cy.get(".oxd-dropdown-menu > :nth-child(1)").click();
-    cy.get(".oxd-autocomplete-text-input > input").type(
-      "Apache Software Foundation"
-    );
-    cy.wait(2000);
 
-    cy.get('div[role="listbox"]')
-      .find(".oxd-autocomplete-option")
-      .each(($element, index, $list) => {
-        if ($element.text().includes("Apache Software Foundation"))
-          cy.wrap($element).click();
-      });
-    cy.get(
-      ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon"
-    ).click();
-    cy.get('input[placeholder="From"]').type("2021-01-01");
+    // cy.get(".oxd-dropdown-menu > :nth-child(1)").click();
+    // cy.get(".oxd-autocomplete-text-input > input").type(
+    //   "Apache Software Foundation"
+    // );
+    // cy.wait(2000);
 
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input"
-    );
-    cy.get('input[placeholder="To"]').type("2023-12-31");
-    cy.get('button[type="submit"]').click({ force: true });
-    
-    cy.fixture("report").then((categories) => {
-      categories.categories.forEach((category) => {
-        const xpathExpression = `//div[normalize-space()="${category}"]`;
-        
-        if (category == "Support & Maintenance") {
-            //cy.xpath(xpathExpression).should("not.be.visible","Maintenance spell mistake");
-            cy.log(`${category} is not listed, Maintenance spell mistake`); 
-            //Maintenance is typo as "Maintanence" on page
-          } else {
-            cy.xpath(xpathExpression).should("be.visible");
-            cy.log(`${category} is listed`);
-          }
-          
-      });
-    });
+    // cy.get('div[role="listbox"]')
+    //   .find(".oxd-autocomplete-option")
+    //   .each(($element, index, $list) => {
+    //     if ($element.text().includes("Apache Software Foundation"))
+    //       cy.wrap($element).click();
+    //   });
+    const timeReport = new TimeReport();
+    timeReport.setProjectName("Apache Software Foundation");
+
+    // cy.get(
+    //   ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon"
+    // ).click();
+    // cy.fixture("report").then((data) =>{
+    //   cy.get('input[placeholder="From"]').type(data.fromDate);
+    // })
+    timeReport.setFromProjectDate();
+
+    // cy.get(
+    //   ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input"
+    // );
+    // cy.fixture("report").then((data) =>{
+    // cy.get('input[placeholder="To"]').type(data.toDate);
+    // })
+    timeReport.setToProjectName();
+
+    // cy.get('button[type="submit"]').click({ force: true });
+    timeReport.clickViewButton();
+
+    // cy.fixture("report").then((data) => {
+    //   data.categories.forEach((category) => {
+    //     const xpathExpression = `//div[normalize-space()="${category}"]`;
+
+    //     if (category == "Support & Maintenance") {
+    //       cy.log(`${category} is not listed, Maintenance spell mistake`);
+    //       //Maintenance is typo as "Maintanence" on page
+    //     } else {
+    //       cy.xpath(xpathExpression).should("be.visible");
+    //       cy.log(`${category} is listed`);
+    //     }
+    //   });
+    // });
+    timeReport.VerifyProjectTimeReport();
   });
 });
---
+++ cypress/support/pageObjects/TimeReport.js
@@ -0,0 +1,56 @@
+class TimeReport{
+
+    setProjectName(projectName){
+        cy.get(".oxd-dropdown-menu > :nth-child(1)").click();
+    cy.get(".oxd-autocomplete-text-input > input").type(projectName);
+    cy.wait(2000);
+
+    cy.get('div[role="listbox"]')
+      .find(".oxd-autocomplete-option")
+      .each(($element, index, $list) => {
+        if ($element.text().includes(projectName))
+          cy.wrap($element).click();
+      });
+    }
+
+    setFromProjectDate(){
+        cy.get(
+            ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon"
+          ).click();
+          cy.fixture("report").then((data) =>{
+            cy.get('input[placeholder="From"]').type(data.fromDate);
+          })    
+    }
+    
+    setToProjectName(){
+        cy.get(
+            ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input"
+          );
+          cy.fixture("report").then((data) =>{
+          cy.get('input[placeholder="To"]').type(data.toDate);
+          })  
+    }
+
+    clickViewButton(){
+        cy.get('button[type="submit"]').click({ force: true });
+    }
+
+    VerifyProjectTimeReport(){
+        cy.fixture("report").then((data) => {
+            data.categories.forEach((category) => {
+            const xpathExpression = `//div[normalize-space()="${category}"]`;
+            
+            if (category == "Support & Maintenance") {
+                cy.log(`${category} is not listed, Maintenance spell mistake`); 
+                //Maintenance is typo as "Maintanence" on page
+              } 
+            else {
+                cy.xpath(xpathExpression).should("be.visible");
+                cy.log(`${category} is listed`);
+              }
+            })
+        })
+}
+}
+
+export default TimeReport;
