5d98f9ec506e7bb8b2999c3e999a26c80f084fa0
lilyliu2021 <lily.liu201209@gmail.com>
2024-01-17 17:47:08 -0800

    deleted commended code

--- cypress/e2e/timeReport.cy.js
+++ cypress/e2e/timeReport.cy.js
@@ -16,54 +16,16 @@ describe("Project Time Report Testing", () => {
       );
     });
     cy.get('li[class*="topbar"]').contains("Reports").click();
-
-    // cy.get(".oxd-dropdown-menu > :nth-child(1)").click();
-    // cy.get(".oxd-autocomplete-text-input > input").type(
-    //   "Apache Software Foundation"
-    // );
-    // cy.wait(2000);
-
-    // cy.get('div[role="listbox"]')
-    //   .find(".oxd-autocomplete-option")
-    //   .each(($element, index, $list) => {
-    //     if ($element.text().includes("Apache Software Foundation"))
-    //       cy.wrap($element).click();
-    //   });
+    
     const timeReport = new TimeReport();
     timeReport.setProjectName("Apache Software Foundation");
 
-    // cy.get(
-    //   ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon"
-    // ).click();
-    // cy.fixture("report").then((data) =>{
-    //   cy.get('input[placeholder="From"]').type(data.fromDate);
-    // })
     timeReport.setFromProjectDate();
 
-    // cy.get(
-    //   ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input"
-    // );
-    // cy.fixture("report").then((data) =>{
-    // cy.get('input[placeholder="To"]').type(data.toDate);
-    // })
     timeReport.setToProjectName();
 
-    // cy.get('button[type="submit"]').click({ force: true });
     timeReport.clickViewButton();
 
-    // cy.fixture("report").then((data) => {
-    //   data.categories.forEach((category) => {
-    //     const xpathExpression = `//div[normalize-space()="${category}"]`;
-
-    //     if (category == "Support & Maintenance") {
-    //       cy.log(`${category} is not listed, Maintenance spell mistake`);
-    //       //Maintenance is typo as "Maintanence" on page
-    //     } else {
-    //       cy.xpath(xpathExpression).should("be.visible");
-    //       cy.log(`${category} is listed`);
-    //     }
-    //   });
-    // });
     timeReport.VerifyProjectTimeReport();
   });
 });
